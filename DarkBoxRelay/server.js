91ee9ffb984a118e31acdef3e7f393d035ae69dd
Taywon Olegaw <thespartan18@hotmail.fr>
2018-05-22 19:47:12 +0200

    changement de port

--- DarkBoxRelay/server.js
+++ DarkBoxRelay/server.js
@@ -1,6 +1,7 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
+console.log("Lancement de l'application sur le port: " + port + "...");
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
